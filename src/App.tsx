1729da6a11995f21a4e80137258457c63fe991ae
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-03-11 23:51:33 +0100

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Routes,Route} from 'react-router-dom'
 import  { Container } from 'react-bootstrap'
+import Store from './pages/Store'
+import About from './pages/About'
+import Home from './pages/Home'
 
 
 function App() {
@@ -9,6 +12,11 @@ function App() {
     <div className="App">
       <Container>
       <h2>hallo</h2>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/store' element={<Store/>}/>
+      <Route path='/about' element={<About/>}/>
+      </Routes>
       </Container>
       
     </div>
