9e18319f32f874ac3bf132470054351adb008506
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-03-14 23:04:21 +0100

    store

--- src/commponents/Storeitem.tsx
+++ src/commponents/Storeitem.tsx
@@ -5,7 +5,7 @@ type StoreItemProp = {
     imgUrl:string
 
 }
-import { Card} from 'react-bootstrap'
+import { Card, } from 'react-bootstrap'
 
 
 const Storeitem = ({id,name,price,imgUrl}:StoreItemProp) => {
@@ -16,11 +16,15 @@ const Storeitem = ({id,name,price,imgUrl}:StoreItemProp) => {
             variant='top'
             src={imgUrl}
             height='200px'
-           style={{ objectFit:'cover'}}
+           style={{ objectFit:'cover'}}/>
 
-            >
+            <Card.Body className='d-flex flex-column'>
+                <Card.Title className='d-flex justify-content-space-between aligin-items-baseline mb-4'>
+                    <span className='fs-2'>{name}</span>
+                    <span className='ms-2 text-muted'>{price}</span>
+                </Card.Title>
 
-            </Card.Img>
+            </Card.Body>
 
         </Card>
     </div>
